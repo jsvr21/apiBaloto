a5cfe389bf24c7f9123d87fabd90fa350ce9c0c7
jsvr21 <152411062+jsvr21@users.noreply.github.com>
2024-09-27 08:33:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+header('Content-Type: application/json');  // Establecer el tipo de contenido a JSON
+
+// URL y token
 $token = 'e517cabd-218b-4611-a804-20db421607d6';  // Tu token de autorización
 $url = 'https://baloto.com/api/v1/results/full/';
 
+// Inicializar cURL
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -10,16 +14,20 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'User-Agent: MyCustomUserAgent/1.0'
 ]);
 
+// Ejecutar la solicitud cURL
 $response = curl_exec($ch);
-// Obtener el código de respuesta HTTP
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);  // Obtener el código HTTP
 
 if (curl_errno($ch)) {
-    // Manejar errores de curl
-    echo 'Error:' . curl_error($ch);
+    // Manejar errores de cURL
+    $error = ['error' => curl_error($ch)];
+    echo json_encode($error, JSON_PRETTY_PRINT);
 } else {
-    echo json_encode($response, JSON_PRETTY_PRINT);  // Muestra la respuesta
+    // Responder con el código HTTP y la respuesta de la API
+    $result = json_decode($response, true);// Decodificar la respuesta JSON de la API
+    echo json_encode($result, JSON_PRETTY_PRINT);
 }
 
+// Cerrar cURL
 curl_close($ch);
 ?>
